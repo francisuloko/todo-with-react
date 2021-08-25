daeaa3a2ef61673dca1f8f211ed507d1d5224aff
Francis Uloko <francisuloko@yahoo.com>
2021-08-26 00:06:38 +0100

    Create react app following react tutorial

---
+++ src/components/Navbar.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+
+  const links = [
+    {
+      id: 1,
+      path: '/',
+      text: 'Home',
+    },
+    {
+      id: 2,
+      path: '/about',
+      text: 'About',
+    },
+  ];
+
+  const handleToggle = () => {
+    setNavbarOpen(!navbarOpen);
+  };
+
+  const closeMenu = () => {
+    setNavbarOpen(false);
+  };
+
+  return (
+    <nav className="navBar">
+      <button onClick={handleToggle}>
+        {}
+      </button>
+      <ul className={`menuNav ${navbarOpen ? ' showMenu' : ''}`}>
+        {links.map((link) => (
+          <li key={link.id}>
+            <NavLink
+              to={link.path}
+              activeClassName="active-link"
+              onClick={() => closeMenu()}
+              exact
+            >
+              {link.text}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+export default Navbar;
